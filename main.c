34d5a1e5503adee6127e42a8bf55cef6d846944f
muzykos <muzykos2001@o2.pl>
2023-04-16 17:36:07 +0200

    Log & stuff

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -19,14 +22,21 @@ int main(int argc, char const *argv[])
     
     umask(0);
 
-    // open log
+    openlog ("loglog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
 
     sid = setsid();
     if (sid < 0) {
+        perror("Sid set error");
+        syslog(LOG_INFO,"Sid setting error");
         exit(EXIT_FAILURE);
     }
 
     // change direc to param
+    if(chdir(argv[1]) < 0){
+        perror("chdir error");
+        syslog(LOG_INFO,"chdir error");
+        exit(EXIT_FAILURE);
+    }
 
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
